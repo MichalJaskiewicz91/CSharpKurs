f8e6575cdc1308ca99c5d47cd44effbd271325c1
Michal <MichalJaskiewicz91@gmail.com>
2019-04-02 11:21:36 +0200

    Overloads

---
+++ Members/Diary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    public class Diary
+    {
+        public Diary()
+        {
+            //inicjalizacja listy
+            ratings = new List<float>();
+            count++;
+        }
+        //Stan(zmienne - pola)
+        private List<float> ratings;
+        public static float minGrade = 0;
+        public static float maxGrade = 10;
+        public static long count = 0;
+        public string Name;
+
+        //Zachowania
+        public void addRating(float rating)
+        {
+            if (rating >= 0 && rating <= 10)
+            {
+                ratings.Add(rating);
+            }
+        }
+        public DiaryStatistics computeStatistics()
+        {
+            DiaryStatistics stats = new DiaryStatistics();
+
+            float sum = 0;
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+            stats.averageGrade = sum / ratings.Count();
+            stats.maxGrade = ratings.Max();
+            stats.minGrade = ratings.Min();
+
+            return stats;
+            
+        }
+    }
+}
---
+++ Members/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StudentDiary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();
+            diary.addRating(5.5f);
+            diary.addRating(6.5f);
+            diary.addRating(7.5f);
+
+            DiaryStatistics stats = diary.computeStatistics();
+            WriteResult("Average",stats.averageGrade,4,5,6,7);
+            WriteResult("Max Value", (int)stats.maxGrade);
+            WriteResult("Min Value", (long)stats.minGrade,2);
+            WriteResult("Min Value", (float)stats.minGrade,1);
+
+            Console.ReadKey();
+        }
+        static void WriteResult(string description, params float[] result)
+        {
+            Console.WriteLine(description + ": " + result[0] + " " + result[1] + " " + result[2] + " " + result[3]);
+        }
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+        static void WriteResult(string description, long result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+        static void WriteResult(string description, float result, long test)
+        {
+            Console.WriteLine("{0}"+ ": " + "{1:F2},{2:},{3},{4}", description,result,1,2,3);
+        }
+        static void WriteResult(string description, long result, long test)
+        {
+            Console.WriteLine($"{description},{result:F2},{2},{3},{4}");
+        }
+    }
+}
